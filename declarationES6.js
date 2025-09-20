8a43b32a2cab933ff0f104d4f39025f99a656973
mounia <mounia.mohamadi@gmail.com>
2025-09-20 13:29:36 +0100

    Mon commit après correction

---
+++ declarationES6.js
@@ -0,0 +1,30 @@
+function exemplevar(){
+    var x=5
+    if (true) {
+        var x=10
+        console.log(x,"exemplevar")
+        
+    } 
+    console.log(x)
+        
+    
+}
+exemplevar()
+
+function ExempleLet(){
+    let y=5
+    if (true) {
+        let y=10
+        console.log(y,"ExempleLet")
+        
+    } 
+    console.log(y)
+         
+}
+ExempleLet()
+
+function ExempleConst(){
+    const z= 5
+    console.log(z)
+}
+ExempleConst()
